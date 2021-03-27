6c356b7043fc0a640026cd7bdab415344cc98a1b
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-03-27 20:31:08 +0800

    remove ml dependency

--- test_upload.py
+++ test_upload.py
@@ -2,7 +2,7 @@ import requests
 import os, shutil
 
 import sys
-import library.ml as ml
+from urllib.parse import urlencode
 
 
 def make_archive(source, destination):
@@ -14,6 +14,7 @@ def make_archive(source, destination):
     shutil.make_archive(name, format, archive_from, archive_to)
     shutil.move('%s.%s' % (name, format), destination)
 
+
 # trigger rebuild
 if __name__ == '__main__':
     project_name = 'blogserver'
@@ -22,8 +23,8 @@ if __name__ == '__main__':
                  'default.zip')
     files = {'file': open('default.zip', 'rb')}
     values = {}
-    pre = ml.url_encode('sh kill.sh')
-    aft = ml.url_encode('sh run.sh')
+    pre = urlencode('sh kill.sh')
+    aft = urlencode('sh run.sh')
     r = requests.post(f'http://kuroweb.cf:8083/upload?pre={pre}&aft={aft}&app=' + f'{project_name}&token={sys.argv[1]}',
                       files=files,
                       data=values)
